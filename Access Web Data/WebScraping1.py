c7906412074a127bb699cb287e6a82ef5d3d1e18
Adith A Danthi <adith.danthi@gmail.com>
2020-05-04 22:37:15 +0530

    Web Scrapping

---
+++ Access Web Data/WebScraping1.py
@@ -0,0 +1,35 @@
+"""
+
+Scraping Numbers from HTML using BeautifulSoup
+The program will use urllib to read the HTML from the data files below,
+and parse the data, extracting numbers and compute the
+sum of the numbers in the file.
+We provide two files for this assignment.
+One is a sample file where we give you the sum for your testing and
+the other is the actual data you need to process for the assignment.
+Sample data: http://python-data.dr-chuck.net/comments_42.html (Sum=2553)
+Actual data: http://python-data.dr-chuck.net/comments_353539.html (Sum ends with 63)
+You do not need to save these files to your folder since your program
+will read the data directly from the URL. Note: Each student will have a
+distinct data url for the assignment - so only use your own data url for analysis.
+
+"""
+
+import urllib.request as ur
+from bs4 import *
+
+url = input('Enter the url to scrape - ')
+
+html = ur.urlopen(url).read()
+soup = BeautifulSoup(html, 'html.parser')
+
+count = 0
+total = 0
+
+spans = soup('span')
+for span in spans:
+    total += int(span.contents[0])
+    count += 1
+
+print('Count ', count)
+print('Sum ', total)
